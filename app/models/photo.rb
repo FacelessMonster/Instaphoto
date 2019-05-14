0edb06d0b43e6f50046225e1aa784f71f5764ee0
FacelessMonster <marsthegod@protonmail.com>
2019-05-15 00:49:55 +0300

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,4 @@ class Photo < ApplicationRecord
   has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :image, presence: true
-  has_attached_file :picture, :storage => :s3, :url => ":s3_domain_url", :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "picture/:id/:filename"
 end
