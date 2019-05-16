33c34bc6281a2f2f5e08f251299352a7e1bc12e9
FacelessMonster <marsthegod@protonmail.com>
2019-05-16 16:17:32 +0300

    update model photo for development mode

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,15 @@
 class Photo < ApplicationRecord
   belongs_to :user
+
   has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  validates :image, presence: true
+  validates_attachment :image, presence: true
+
+  if Rails.env.development?
+    has_attached_file :image,
+    :path => ":rails_root/public/photos/system/:attachment/:id/:style/:filename",
+    :url => "/photos/system/:attachment/:id/:style/:filename",
+    styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }
+  end
+
 end
