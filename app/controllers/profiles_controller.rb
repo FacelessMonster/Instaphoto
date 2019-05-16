5cee2a963978956122205756c46db24080e12288
FacelessMonster <marsthegod@protonmail.com>
2019-05-16 23:03:29 +0300

    add subscriptions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,27 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: [:show]
+  before_action :set_profile, only: [:show, :subscribe, :unsubscribe]
+
+  def subscribe
+    if current_user.subscriptions.find_by_friend_id(@profile.id).nil?
+      @subscription = current_user.subscriptions.build
+      @subscription.friend_id = @profile.id
+      if @subscription.save
+        redirect_to profile_path(@profile), notice: "Subscribed."
+      end
+    else
+      redirect_to profile_path(@profile), notice: "You are already subscribed."
+    end
+  end
+
+  def unsubscribe
+    @subscription = current_user.subscriptions.find_by_friend_id(@profile.id)
+    unless @subscription.nil?
+      @subscription.destroy
+      redirect_to profile_path(@profile), notice: "Unsubscribed."
+      else 
+        redirect_to profile_path(@profile), notice: "You have not been subscribed."
+    end
+  end
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'pages#landing'
   get 'pages/about'
   get 'profiles/:id' => 'profiles#show', as: 'profile'
+  get 'profiles/:id/subscribe' => 'profiles#subscribe', as: 'subscribe'
+  get 'profiles/:id/unsubscribe' => 'profiles#unsubscribe', as: 'unsubscribe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
