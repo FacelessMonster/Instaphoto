9cb2782805eaf20a465c2cb2deef12a3038e5836
FacelessMonster <marsthegod@protonmail.com>
2019-05-19 20:06:52 +0300

    setup actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://kytak.herokuapp.com/', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'kytak.herokuapp.com',
+    user_name:            ENV.fetch('MAIL_ACCOUNT_NAME'),
+    password:             ENV.fetch('MAIL_PASSWORD'),
+    authentication:       'plain',
+    enable_starttls_auto: true }
 end
